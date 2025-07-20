29b029bd70b68e0980520831cccb5f01502f2b6f
Carlo Renoria <itechcarlo@gmail.com>
2025-07-20 10:13:36 +0800

    Create Login

--- App.tsx
+++ App.tsx
@@ -5,24 +5,39 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StyleSheet, useColorScheme } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./views/LoginScreen";
+import HomeScreen from "./views/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+	const isDarkMode = useColorScheme() === "dark";
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+	return (
+		// <View style={styles.container}>
+		// 	<StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+		// 	<NewAppScreen templateFileName="App.tsx" />
+		// </View>
+		// <LoginView />
+		<NavigationContainer>
+			<Stack.Navigator
+				initialRouteName="Login"
+				screenOptions={{ headerShown: false }}
+			>
+				<Stack.Screen name="Login" component={LoginScreen} />
+				<Stack.Screen name="Home" component={HomeScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+	container: {
+		flex: 1,
+	},
 });
 
 export default App;
