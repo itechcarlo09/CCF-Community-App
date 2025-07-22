e813c1c1a5406e247e543629a3f7a64ca4285237
Carlo Renoria <itechcarlo@gmail.com>
2025-07-22 21:54:08 +0800

    apply refresh in the list of users

--- views/User/UserScreen.tsx
+++ views/User/UserScreen.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, {
+	useCallback,
+	useEffect,
+	useLayoutEffect,
+	useState,
+} from "react";
 import {
 	View,
 	Text,
@@ -8,6 +13,7 @@ import {
 	ActivityIndicator,
 	FlatList,
 	TouchableOpacity,
+	RefreshControl,
 } from "react-native";
 import { getApp } from "@react-native-firebase/app";
 import {
@@ -15,40 +21,39 @@ import {
 	collection,
 	getDocs,
 } from "@react-native-firebase/firestore";
-
-type User = {
-	id: string;
-	FirstName: string;
-	MiddleName: string;
-	LastName: string;
-};
+import { User } from "../../firebase/firestore/types/User";
 
 const UserScreen = ({ navigation }: any) => {
 	const app = getApp();
 	const db = getFirestore(app);
 	const [events, setEvents] = useState<User[]>([]);
 	const [loading, setLoading] = useState(true);
+	const [refreshing, setRefreshing] = useState(false);
 
-	useEffect(() => {
-		const fetchEvents = async () => {
-			try {
-				const snapshot = await getDocs(collection(db, "Users"));
-				const userList: User[] = snapshot.docs.map((doc: any) => ({
-					id: doc.id,
-					...doc.data(),
-				})) as User[];
-				setEvents(userList);
-			} catch (error) {
-				console.error("Error fetching users:", error);
-			} finally {
-				setLoading(false);
-			}
-		};
+	const fetchUsers = async () => {
+		try {
+			const snapshot = await getDocs(collection(db, "Users"));
+			const userList: User[] = snapshot.docs.map((doc: any) => ({
+				id: doc.id,
+				...doc.data(),
+			})) as User[];
+			setEvents(userList);
+		} catch (error) {
+			console.error("Error fetching users:", error);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-		fetchEvents();
+	const onRefresh = useCallback(async () => {
+		setRefreshing(true);
+		await fetchUsers();
+		setRefreshing(false);
 	}, []);
 
-	console.log(events);
+	useEffect(() => {
+		fetchUsers();
+	}, []);
 
 	if (loading) {
 		return <ActivityIndicator size="large" style={styles.loader} />;
@@ -65,11 +70,15 @@ const UserScreen = ({ navigation }: any) => {
 						onPress={() => console.log(item.id)}
 					>
 						<Text style={styles.text}>
-							🎉 Welcome to Elevate {item.FirstName} {item.MiddleName[0]}.{" "}
-							{item.LastName}!
+							🎉 Welcome to Elevate {item.firstName}{" "}
+							{item.middleName ? item.middleName[0] + ". " : ""}
+							{item.lastName}!
 						</Text>
 					</TouchableOpacity>
 				)}
+				refreshControl={
+					<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+				}
 			/>
 		</View>
 	);
