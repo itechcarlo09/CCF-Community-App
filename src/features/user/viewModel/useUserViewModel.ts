37cb8b12f925f0fd988cf14154ae3573e26fd18e
Carlo Renoria <itechcarlo@gmail.com>
2026-02-26 11:35:32 +0800

    Add top users data and refactor user mapping logic to include dLeader names

--- src/features/user/viewModel/useUserViewModel.ts
+++ src/features/user/viewModel/useUserViewModel.ts
@@ -5,39 +5,65 @@ import { ageNumber } from "../../../utils/dateFormatter";
 import { formatFullName } from "../../../utils/stringUtils";
 import { RecordItemUI } from "../model/RecordListItem";
 import { DropdownOption } from "../../../types/dropdownOption";
+import topUsers from "../topUsers.json";
+import { MembershipType } from "../types";
 
 const MAX_AGE_FOR_ELEVATE = 22;
 const MIN_DGROUP_MEMBERS_FOR_DLEADER = 3;
 const MIN_DGROUP_MEMBERS_FOR_TIMOTHY = 1;
 
-const mapUserToUI = (user: User): RecordItemUI => ({
-	id: user.id,
-	fallbackText: `${user.firstName[0]}${user.lastName[0]}`,
-	fullName: formatFullName(user.firstName, user.lastName, user.middleName),
-	age: ageNumber(user.birthDate),
-	ministryText:
-		ageNumber(user.birthDate) <= MAX_AGE_FOR_ELEVATE
-			? "ELEVATE Youth"
-			: "B1G Singles",
-	status: "Active Member",
-	dleaderName: user.dGroupLeader
-		? formatFullName(
-				user.dGroupLeader.firstName,
-				user.dGroupLeader.lastName,
-				user.dGroupLeader.middleName,
-		  )
-		: null,
-	membershipType:
-		user?.dGroupMembers &&
-		user.dGroupMembers.length >= MIN_DGROUP_MEMBERS_FOR_DLEADER
-			? "DLeader"
-			: user?.dGroupMembers &&
-			  user.dGroupMembers.length >= MIN_DGROUP_MEMBERS_FOR_TIMOTHY
-			? "Timothy"
-			: user.dGroupLeader
-			? "DMember"
-			: "Pending Member",
-});
+const mapUserToUI = (user: User): RecordItemUI => {
+	const fallbackText = `${user.firstName[0]}${user.lastName[0]}`;
+	const fullName = formatFullName(
+		user.firstName,
+		user.lastName,
+		user.middleName,
+	);
+	const age = ageNumber(user.birthDate);
+
+	const ministryText =
+		age <= MAX_AGE_FOR_ELEVATE ? "ELEVATE Youth" : "B1G Singles";
+
+	const topUser = topUsers.find((topUser) => topUser.id === user.id);
+	const dleaderName =
+		topUser?.dleaderName ??
+		(user.dGroupLeader
+			? formatFullName(
+					user.dGroupLeader.firstName,
+					user.dGroupLeader.lastName,
+					user.dGroupLeader.middleName,
+			  )
+			: null);
+
+	let membershipType: MembershipType;
+	if (
+		topUser ||
+		(user.dGroupMembers &&
+			user.dGroupMembers.length >= MIN_DGROUP_MEMBERS_FOR_DLEADER)
+	) {
+		membershipType = "DLeader";
+	} else if (
+		user.dGroupMembers &&
+		user.dGroupMembers.length >= MIN_DGROUP_MEMBERS_FOR_TIMOTHY
+	) {
+		membershipType = "Timothy";
+	} else if (user.dGroupLeader) {
+		membershipType = "DMember";
+	} else {
+		membershipType = "Pending Member";
+	}
+
+	return {
+		id: user.id,
+		fallbackText,
+		fullName,
+		age,
+		ministryText,
+		status: "Active Member",
+		dleaderName,
+		membershipType,
+	};
+};
 
 const mapUserToDLeadersList = (user: User): DropdownOption => ({
 	label: formatFullName(user.firstName, user.lastName, user.middleName),
